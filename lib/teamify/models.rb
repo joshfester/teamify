e527ba6dd7b53fa2a01588aad358e31173125d70
Josh Fester <joshfester@gmail.com>
2024-01-15 22:18:05 +0000

    refactor Models

--- lib/teamify/models.rb
+++ lib/teamify/models.rb
@@ -1,44 +1,31 @@
 module Teamify
   module Models
-    DEFAULT_TEAMIFY_TEAM_CLASS = "Team"
-    DEFAULT_TEAMIFY_MEMBERSHIP_CLASS = "Membership"
-    DEFAULT_TEAMIFY_MEMBER_CLASS = "User"
+    DEFAULT_TEAM_CLASS = "Team"
+    DEFAULT_MEMBERSHIP_CLASS = "Membership"
+    DEFAULT_MEMBER_CLASS = "User"
 
     def teamify(model, **kwargs)
-      case model
+      concern = case model
       when :team
-        teamify_team kwargs
+        Teams::Base
       when :membership
-        teamify_membership kwargs
+        Memberships::Base
       when :member
-        teamify_member kwargs
+        Members::Base
       else
         raise "Teamify:: invalid model type '#{model}'"
       end
+
+      teamify_constants concern, kwargs
+      include concern
     end
 
     private
 
-    def teamify_team(options)
-      options[:membership_class] ||= DEFAULT_TEAMIFY_MEMBERSHIP_CLASS
-      options[:member_class] ||= DEFAULT_TEAMIFY_MEMBER_CLASS
-      Teams::Base.const_set :TEAMIFY_MEMBERSHIP_CLASS, options[:membership_class]
-      Teams::Base.const_set :TEAMIFY_MEMBER_CLASS, options[:member_class]
-      include Teams::Base
-    end
-
-    def teamify_membership(options)
-      options[:team_class] ||= DEFAULT_TEAMIFY_TEAM_CLASS
-      Memberships::Base.const_set :TEAMIFY_TEAM_CLASS, options[:team_class]
-      include Memberships::Base
-    end
-
-    def teamify_member(options)
-      options[:team_class] ||= DEFAULT_TEAMIFY_TEAM_CLASS
-      options[:membership_class] ||= DEFAULT_TEAMIFY_MEMBERSHIP_CLASS
-      Members::Base.const_set :TEAMIFY_TEAM_CLASS, options[:team_class]
-      Members::Base.const_set :TEAMIFY_MEMBERSHIP_CLASS, options[:membership_class]
-      include Members::Base
+    def teamify_constants(concern, options)
+      concern.const_set :TEAMIFY_TEAM_CLASS, options[:team_class] || DEFAULT_TEAM_CLASS
+      concern.const_set :TEAMIFY_MEMBERSHIP_CLASS, options[:membership_class] || DEFAULT_MEMBERSHIP_CLASS
+      concern.const_set :TEAMIFY_MEMBER_CLASS, options[:member_class] || DEFAULT_MEMBER_CLASS
     end
   end
 end
