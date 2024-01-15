f46606550a0c6c65024adcc459960dc5dbe7c0ca
Josh Fester <joshfester@gmail.com>
2024-01-15 17:33:42 +0000

    refactor Teams::Base and Models

--- app/models/concerns/teams/base.rb
+++ app/models/concerns/teams/base.rb
@@ -3,10 +3,13 @@ module Teams
     extend ActiveSupport::Concern
 
     included do
-      has_many :memberships, dependent: :destroy
-      has_many :users, through: :memberships
+      has_many :memberships,
+        dependent: :destroy,
+        class_name: TEAMIFY_MEMBERSHIP_CLASS,
+        foreign_key: :team_id
+
+      has_many :members, through: :memberships
       has_many :invitations
     end
-
   end
 end
--- lib/teamify/models.rb
+++ lib/teamify/models.rb
@@ -1,13 +1,11 @@
 module Teamify
   module Models
-
     def teamify(model, **kwargs)
       case model
       when :team
-        include Teams::Base 
+        teamify_team kwargs
       when :membership
         teamify_membership kwargs
-        include Memberships::Base 
       else
         raise "Teamify:: invalid model type '#{model}'"
       end
@@ -15,10 +13,16 @@ module Teamify
 
     private
 
-    def teamify_membership(options)
-      options[:team_model] ||= "Team"
-      Memberships::Base.const_set :TEAMIFY_TEAM_CLASS, options[:team_model]
+    def teamify_team(options)
+      options[:membership_class] ||= "Membership"
+      Teams::Base.const_set :TEAMIFY_MEMBERSHIP_CLASS, options[:membership_class]
+      include Teams::Base
     end
 
+    def teamify_membership(options)
+      options[:team_class] ||= "Team"
+      Memberships::Base.const_set :TEAMIFY_TEAM_CLASS, options[:team_class]
+      include Memberships::Base
+    end
   end
 end
